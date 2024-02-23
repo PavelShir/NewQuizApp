0cfd39355b3291cd5b3e9ec3276f7a3a17d8e7d8
Pavel Shirokiy <pshirokiy@gmail.com>
2024-02-23 14:19:14 +0300

    do the timer delay for button

--- NewQuizApp/ViewController.swift
+++ NewQuizApp/ViewController.swift
@@ -16,24 +16,25 @@ class ViewController: UIViewController {
     @IBOutlet var progressBar: UIProgressView!
     
     let questions = [
-        ["A slug's blood is green.","True"],
-        ["Approximately one quarter of human bones are in the feet.","True"],
-        ["The total surface area of two human lungs is approximately 70 square metres.","True"],
-        ["In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.","True"],
-        ["In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.","False"],
-        ["It is illegal to pee in the Ocean in Portugal.","True"],
-        ["You can lead a cow down stairs but not up stairs.","False"],
-        ["Google was originally called 'Backrub'.","True"],
-        ["Buzz Aldrin's mother's maiden name was 'Moon'.","True"],
-        ["The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.","False"],
-        ["No piece of square dry paper can be folded in half more than 7 times.","False"],
-        ["Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.","True"]
+        Question(q: "A slug's blood is green.", a: "True"),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
     ]
     
     var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupLabel()
         updateQuestion()
         buttonTrueSetup()
         buttonFalseSetup()
@@ -45,12 +46,12 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed(_ sender: UIButton) {
         
         let answer = sender.currentTitle
-        let correctAnswer = questions[questionNumber][1]
+        let correctAnswer = questions[questionNumber].answer
         
         if answer == correctAnswer {
-            print("Right!")
+            sender.backgroundColor = .systemGreen
         } else {
-            print("Wrong!")
+            sender.backgroundColor = .systemRed
         }
         
         if questionNumber < (questions.count-1) {
@@ -60,14 +61,21 @@ class ViewController: UIViewController {
             questionNumber = 0
         }
         
-        updateQuestion()
+        Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(updateQuestion), userInfo: nil, repeats: false)
+        
     }
     
-    
-    private func updateQuestion() {
-        questionLabel.text = questions[questionNumber][0]
+    @objc private func updateQuestion() {
+        questionLabel.text = questions[questionNumber].text
+        buttonTrue.backgroundColor = .clear
+        buttonFalse.backgroundColor = .clear
     }
     
+    private func setupLabel() {
+        questionLabel.textColor = .systemYellow
+        questionLabel.font = .systemFont(ofSize: 25)
+        questionLabel.numberOfLines = .max
+    }
     
     private func buttonTrueSetup() {
         buttonTrue.layer.borderWidth = 3
